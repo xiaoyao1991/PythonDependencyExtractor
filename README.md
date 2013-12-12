748bca2cc30fb7389eeee055d56228d4be17e24a
Xiaoyao Qian <qian13@illinois.edu>
2013-12-12 00:42:43 -0600

    excluded project dependencies

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 ### Python Package Dependency Extractor
-This script will extract the dependencies of a python file, or a directory that contains python files. This will be useful in either using third-party package that doesn't come with a "requirement.txt", or creating your own "requirement.txt" when porting to server.
+This script will extract the dependencies of a python file, or a directory that contains python files. This will be useful in either using third-party package that doesn't come with a "requirement.txt", or creating your own "requirement.txt" when porting to server. 
 
 #### Usage:
 ./extract-dependencies.py [-r] *<path-to-file-or-dir>* > requirement.txt
+
+
+### Note:
+Currently this script only supports python 2.7. If you want support for other versions of python, just extract the builtin modules list from <http://docs.python.org/2.7/py-modindex.html> and assign the list to the builtin_modules in the **DependencyExtractor** class. Or even easier, simply uncomment the *get_builtin_packages* method.
 
 #### TODO:  
 1. Avoid comment area (Done)
--- extract-dependencies.py
+++ extract-dependencies.py
@@ -2,6 +2,7 @@
 import sys, argparse, urllib2
 from os import listdir
 from os.path import isfile, isdir, lexists, abspath
+# from bs4 import BeautifulSoup
 
 def log_err(*kwargs):
     msg = ''
@@ -11,9 +12,9 @@ def log_err(*kwargs):
     sys.stderr.write(msg + '\n')
 
 
-class DependencyExtracter(object):
+class DependencyExtractor(object):
     def __init__(self, version='2.7', verbose=False):
-        super(DependencyExtracter, self).__init__()
+        super(DependencyExtractor, self).__init__()
         self.version = version
         self.verbose = verbose
         self.builtin_modules = ['\\', '__builtin__', '__future__', '__main__', '_winreg', 'abc', 'aepack', 'aetools', 'aetypes', 'aifc', 'al', 'AL', 'anydbm', 'applesingle', 'argparse', 'array', 'ast', 'asynchat', 'asyncore', 'atexit', 'audioop', 'autoGIL', 'base64', 'BaseHTTPServer', 'Bastion', 'bdb', 'binascii', 'binhex', 'bisect', 'bsddb', 'buildtools', 'bz2', 'calendar', 'Carbon', 'Carbon.AE', 'Carbon.AH', 'Carbon.App', 'Carbon.Appearance', 'Carbon.CarbonEvents', 'Carbon.CarbonEvt', 'Carbon.CF', 'Carbon.CG', 'Carbon.Cm', 'Carbon.Components', 'Carbon.ControlAccessor', 'Carbon.Controls', 'Carbon.CoreFounation', 'Carbon.CoreGraphics', 'Carbon.Ctl', 'Carbon.Dialogs', 'Carbon.Dlg', 'Carbon.Drag', 'Carbon.Dragconst', 'Carbon.Events', 'Carbon.Evt', 'Carbon.File', 'Carbon.Files', 'Carbon.Fm', 'Carbon.Folder', 'Carbon.Folders', 'Carbon.Fonts', 'Carbon.Help', 'Carbon.IBCarbon', 'Carbon.IBCarbonRuntime', 'Carbon.Icns', 'Carbon.Icons', 'Carbon.Launch', 'Carbon.LaunchServices', 'Carbon.List', 'Carbon.Lists', 'Carbon.MacHelp', 'Carbon.MediaDescr', 'Carbon.Menu', 'Carbon.Menus', 'Carbon.Mlte', 'Carbon.OSA', 'Carbon.OSAconst', 'Carbon.Qd', 'Carbon.Qdoffs', 'Carbon.QDOffscreen', 'Carbon.Qt', 'Carbon.QuickDraw', 'Carbon.QuickTime', 'Carbon.Res', 'Carbon.Resources', 'Carbon.Scrap', 'Carbon.Snd', 'Carbon.Sound', 'Carbon.TE', 'Carbon.TextEdit', 'Carbon.Win', 'Carbon.Windows', 'cd', 'cfmfile', 'cgi', 'CGIHTTPServer', 'cgitb', 'chunk', 'cmath', 'cmd', 'code', 'codecs', 'codeop', 'collections', 'ColorPicker', 'colorsys', 'commands', 'compileall', 'compiler', 'compiler.ast', 'compiler.visitor', 'ConfigParser', 'contextlib', 'Cookie', 'cookielib', 'copy', 'copy_reg', 'cPickle', 'cProfile', 'crypt', 'cStringIO', 'csv', 'ctypes', 'curses', 'curses.ascii', 'curses.panel', 'curses.textpad', 'datetime', 'dbhash', 'dbm', 'decimal', 'DEVICE', 'difflib', 'dircache', 'dis', 'distutils', 'distutils.archive_util', 'distutils.bcppcompiler', 'distutils.ccompiler', 'distutils.cmd', 'distutils.command', 'distutils.command.bdist', 'distutils.command.bdist_dumb', 'distutils.command.bdist_msi', 'distutils.command.bdist_packager', 'distutils.command.bdist_rpm', 'distutils.command.bdist_wininst', 'distutils.command.build', 'distutils.command.build_clib', 'distutils.command.build_ext', 'distutils.command.build_py', 'distutils.command.build_scripts', 'distutils.command.check', 'distutils.command.clean', 'distutils.command.config', 'distutils.command.install', 'distutils.command.install_data', 'distutils.command.install_headers', 'distutils.command.install_lib', 'distutils.command.install_scripts', 'distutils.command.register', 'distutils.command.sdist', 'distutils.core', 'distutils.cygwinccompiler', 'distutils.debug', 'distutils.dep_util', 'distutils.dir_util', 'distutils.dist', 'distutils.emxccompiler', 'distutils.errors', 'distutils.extension', 'distutils.fancy_getopt', 'distutils.file_util', 'distutils.filelist', 'distutils.log', 'distutils.msvccompiler', 'distutils.spawn', 'distutils.sysconfig', 'distutils.text_file', 'distutils.unixccompiler', 'distutils.util', 'distutils.version', 'dl', 'doctest', 'DocXMLRPCServer', 'dumbdbm', 'dummy_thread', 'dummy_threading', 'EasyDialogs', 'email', 'email.charset', 'email.encoders', 'email.errors', 'email.generator', 'email.header', 'email.iterators', 'email.message', 'email.mime', 'email.parser', 'email.utils', 'encodings', 'encodings.idna', 'encodings.utf_8_sig', 'errno', 'exceptions', 'fcntl', 'filecmp', 'fileinput', 'findertools', 'FL', 'fl', 'flp', 'fm', 'fnmatch', 'formatter', 'fpectl', 'fpformat', 'fractions', 'FrameWork', 'ftplib', 'functools', 'future_builtins', 'gc', 'gdbm', 'gensuitemodule', 'getopt', 'getpass', 'gettext', 'gl', 'GL', 'glob', 'grp', 'gzip', 'hashlib', 'heapq', 'hmac', 'hotshot', 'hotshot.stats', 'htmlentitydefs', 'htmllib', 'HTMLParser', 'httplib', 'ic', 'icopen', 'imageop', 'imaplib', 'imgfile', 'imghdr', 'imp', 'importlib', 'imputil', 'inspect', 'io', 'itertools', 'jpeg', 'json', 'keyword', 'lib2to3', 'linecache', 'locale', 'logging', 'logging.config', 'logging.handlers', 'macerrors', 'MacOS', 'macostools', 'macpath', 'macresource', 'mailbox', 'mailcap', 'marshal', 'math', 'md5', 'mhlib', 'mimetools', 'mimetypes', 'MimeWriter', 'mimify', 'MiniAEFrame', 'mmap', 'modulefinder', 'msilib', 'msvcrt', 'multifile', 'multiprocessing', 'multiprocessing.connection', 'multiprocessing.dummy', 'multiprocessing.managers', 'multiprocessing.pool', 'multiprocessing.sharedctypes', 'mutex', 'Nav', 'netrc', 'new', 'nis', 'nntplib', 'numbers', 'operator', 'optparse', 'os', 'os.path', 'ossaudiodev', 'parser', 'pdb', 'pickle', 'pickletools', 'pipes', 'PixMapWrapper', 'pkgutil', 'platform', 'plistlib', 'popen2', 'poplib', 'posix', 'posixfile', 'pprint', 'profile', 'pstats', 'pty', 'pwd', 'py_compile', 'pyclbr', 'pydoc', 'Queue', 'quopri', 'random', 're', 'readline', 'repr', 'resource', 'rexec', 'rfc822', 'rlcompleter', 'robotparser', 'runpy', 'sched', 'ScrolledText', 'select', 'sets', 'sgmllib', 'sha', 'shelve', 'shlex', 'shutil', 'signal', 'SimpleHTTPServer', 'SimpleXMLRPCServer', 'site', 'smtpd', 'smtplib', 'sndhdr', 'socket', 'SocketServer', 'spwd', 'sqlite3', 'ssl', 'stat', 'statvfs', 'string', 'StringIO', 'stringprep', 'struct', 'subprocess', 'sunau', 'sunaudiodev', 'SUNAUDIODEV', 'symbol', 'symtable', 'sys', 'sysconfig', 'syslog', 'tabnanny', 'tarfile', 'telnetlib', 'tempfile', 'termios', 'test', 'test.test_support', 'textwrap', 'thread', 'threading', 'time', 'timeit', 'Tix', 'Tkinter', 'token', 'tokenize', 'trace', 'traceback', 'ttk', 'tty', 'turtle', 'types', 'unicodedata', 'unittest', 'urllib', 'urllib2', 'urlparse', 'user', 'UserDict', 'UserList', 'UserString', 'uu', 'uuid', 'videoreader', 'W', 'warnings', 'wave', 'weakref', 'webbrowser', 'whichdb', 'winsound', 'wsgiref', 'wsgiref.handlers', 'wsgiref.headers', 'wsgiref.simple_server', 'wsgiref.util', 'wsgiref.validate', 'xdrlib', 'xml', 'xml.dom', 'xml.dom.minidom', 'xml.dom.pulldom', 'xml.etree.ElementTree', 'xml.parsers.expat', 'xml.sax', 'xml.sax.handler', 'xml.sax.saxutils', 'xml.sax.xmlreader', 'xmlrpclib', 'zipfile', 'zipimport', 'zlib',]
@@ -22,17 +23,14 @@ class DependencyExtracter(object):
         # self.get_builtin_packages()
 
 
-    def get_builtin_packages(self):
-        # html = urllib2.urlopen('http://docs.python.org/2.7/py-modindex.html').read()
-        # soup = BeautifulSoup(html)
-        # module_elems = soup.find_all(class_='xref')
+    # def get_builtin_packages(self):
+    #     self.builtin_modules = []
+    #     html = urllib2.urlopen('http://docs.python.org/%s/py-modindex.html' % '2.7').read()
+    #     soup = BeautifulSoup(html)
+    #     module_elems = soup.find_all(class_='xref')
 
-        # for module_elem in module_elems:
-        #     self.builtin_modules.append(module_elem.text)
-        fp = open('builtin_packages.txt', 'r')
-        for line in fp:
-            self.builtin_modules.append(line.strip())
-        fp.close()
+    #     for module_elem in module_elems:
+    #         self.builtin_modules.append(module_elem.text)
 
 
     def exclude_project_modules(self, fp):
@@ -142,7 +140,7 @@ def main():
         log_err('[Error] "%s" does not exist.' % fp)
         exit(1)
     
-    extractor = DependencyExtracter(verbose=args.verbose)
+    extractor = DependencyExtractor(verbose=args.verbose)
     if args.recursive:
         if not isdir(fp):
             log_err('[Error] "%s" is not a directory.' % fp)
